40e25d49a0f2cd70fbc9d693fffec2082865ae43
mmartine2008 <mariano.andres.martinez@gmail.com>
2021-06-09 17:27:46 -0300

    Envia foto por post

--- js/acceso.js
+++ js/acceso.js
@@ -1,3 +1,20 @@
+function confirmar()
+{
+	try {
+		var canvas = document.getElementById("canvas");
+		var context = canvas.getContext("2d");
+		var img     = canvas.toDataURL("image/png");
+		var imagen = document.getElementById("imagen");
+		imagen.value = img;
+	}
+	catch(err) {
+		console.log(err);		
+	}	
+	
+}
+
 $( document ).ready(function() {
     initCanvas(320,240);
+
+    $('#btEnviar').click(confirmar);
 });
